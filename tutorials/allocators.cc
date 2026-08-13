8e760d2dbf7c26aad477d16eeeaf8034f0050e78
Ryan <ryan.keane051@gmail.com>
2026-08-13 16:57:15 -0400

    Added code for POCMA/POCS section

--- tutorials/allocators.cc
+++ tutorials/allocators.cc
@@ -49,8 +49,9 @@ class Owner {
 
  public:
   Owner() = default;
-  
+
   explicit Owner(const T& obj) : obj_(new T(obj)) {}
+
   explicit Owner(T&& obj) : obj_(new T(std::move(obj))) {}
 
   // Rule-of-five implementation below.
@@ -163,6 +164,7 @@ class Owner {
   Owner() = default;
 
   explicit Owner(const T& obj) : obj_(create(obj)) {}
+
   explicit Owner(T&& obj) : obj_(create(std::move(obj))) {}
 
   // Everything else about the implementation is the same;
@@ -235,7 +237,7 @@ TEST(TutorialsAllocators, SimpleAllocatorOwner) {
 
 // Now, let's allow allocators that hold state, and add
 // allocator-aware constructors. For now, we will remove
-// the ability for Owner to be move assigned or swapped. 
+// the ability for Owner to be move assigned or swapped.
 // We will discuss the complexity of adding these features in the next section.
 namespace xyz::tutorials::stateful_allocator {
 
@@ -295,10 +297,10 @@ class Owner {
   // the style shown below, tagged with allocator_arg_t. The following style
   // works with variadic constructors, whereas the trailing style does not.
   explicit Owner(std::allocator_arg_t, const Alloc& a, const T& obj)
-    : alloc_(a), obj_(create(obj)) {}
+      : alloc_(a), obj_(create(obj)) {}
 
   explicit Owner(std::allocator_arg_t, const Alloc& a, T&& obj)
-    : alloc_(a), obj_(create(std::move(obj))) {}
+      : alloc_(a), obj_(create(std::move(obj))) {}
 
   ~Owner() { destroy(obj_); }
 
@@ -314,8 +316,9 @@ class Owner {
   Owner(std::allocator_arg_t, const Alloc& a, const Owner& other)
       : alloc_(a), obj_(copy(other.obj_)) {}
 
-  Owner(Owner&& other) noexcept : 
-    alloc_(std::move(other.alloc_)), obj_(std::exchange(other.obj_, nullptr)) {}
+  Owner(Owner&& other) noexcept
+      : alloc_(std::move(other.alloc_)),
+        obj_(std::exchange(other.obj_, nullptr)) {}
 
   // Allocator-aware move construction is complex - see the following step.
   Owner(std::allocator_arg_t, const Alloc& a, Owner&& other) = delete;
@@ -325,11 +328,11 @@ class Owner {
   // only reassign our allocator if it is true.
   Owner& operator=(const Owner& other) {
     if (this != &other) {
-      destroy(obj_); // Destroy with the old allocator.
+      destroy(obj_);  // Destroy with the old allocator.
       if constexpr (traits::propagate_on_container_copy_assignment::value) {
         alloc_ = other.alloc_;
       }
-      obj_ = copy(other.obj_); // Construct with the new allocator.
+      obj_ = copy(other.obj_);  // Construct with the new allocator.
     }
     return *this;
   }
@@ -357,16 +360,15 @@ class Owner {
 // allocator.
 template <typename T>
 class TestAlloc {
-public:
+ public:
   using value_type = T;
-  int tag = 0; // Our "state"
+  int tag = 0;  // Our "state"
 
   TestAlloc() = default;
+
   explicit TestAlloc(int tag) : tag(tag) {}
 
-  T* allocate(std::size_t count) {
-    return std::allocator<T>{}.allocate(count);
-  }
+  T* allocate(std::size_t count) { return std::allocator<T>{}.allocate(count); }
 
   void deallocate(T* ptr, std::size_t count) {
     std::allocator<T>{}.deallocate(ptr, count);
@@ -377,7 +379,7 @@ public:
 
 TEST(TutorialsAllocators, StatefulAllocator) {
   using TestOwner = Owner<int, TestAlloc<int>>;
-  
+
   // Default constructs the allocator.
   TestOwner o1;
   EXPECT_EQ(o1.get_allocator().tag, 0);
@@ -387,14 +389,13 @@ TEST(TutorialsAllocators, StatefulAllocator) {
   EXPECT_EQ(o2.get(), 42);
   EXPECT_EQ(o2.get_allocator().tag, 0);
 
-
   // Provides an explicit allocator.
   TestOwner o3{std::allocator_arg, TestAlloc<int>{5}, 10};
-  EXPECT_EQ(o3.get(), 10); // 10 is the value stored in Owner.
-  EXPECT_EQ(o3.get_allocator().tag, 5); // 5 is the value stored in TestAlloc.
+  EXPECT_EQ(o3.get(), 10);               // 10 is the value stored in Owner.
+  EXPECT_EQ(o3.get_allocator().tag, 5);  // 5 is the value stored in TestAlloc.
 
   // Copy assignment does NOT take the allocator by default.
-  // If our allocator set propagate_on_container_copy_assignment to true, 
+  // If our allocator set propagate_on_container_copy_assignment to true,
   // it would take o3's allocator.
   o2 = o3;
   EXPECT_EQ(o2.get(), 10);
@@ -403,14 +404,14 @@ TEST(TutorialsAllocators, StatefulAllocator) {
   // Copy construction copies the allocator.
   TestOwner o4{o3};
   EXPECT_EQ(o4.get(), 10);
-  // If TestAlloc had a unique select_on_container_copy_construction function, 
+  // If TestAlloc had a unique select_on_container_copy_construction function,
   // the tag could differ here.
   EXPECT_EQ(o4.get_allocator().tag, 5);
 
   // Allocator-aware copy construction uses the new allocator.
   TestOwner o5{std::allocator_arg, TestAlloc<int>{20}, o3};
-  EXPECT_EQ(o5.get(), 10);// Take o3's value.
-  EXPECT_EQ(o5.get_allocator().tag, 20); // Use the new allocator.
+  EXPECT_EQ(o5.get(), 10);                // Take o3's value.
+  EXPECT_EQ(o5.get_allocator().tag, 20);  // Use the new allocator.
 
   // Move construction takes the allocator.
   TestOwner o6{std::move(o5)};
@@ -419,3 +420,182 @@ TEST(TutorialsAllocators, StatefulAllocator) {
 }
 
 }  // namespace xyz::tutorials::stateful_allocator
+
+// Finally, we can add the missing operations back to Owner.
+// An object allocated with one allocator must be deallocated
+// with an equivalent allocator. We must pay careful attention
+// to this fact when implementing moves and swaps.
+namespace xyz::tutorials::propagating_allocator {
+
+template <typename T, typename Alloc = std::allocator<T>>
+class Owner {
+  [[no_unique_address]] Alloc alloc_;
+  T* obj_ = nullptr;
+
+  using traits = std::allocator_traits<Alloc>;
+  // Some useful shortenings for common properties of our allocator.
+  constexpr static bool pocma =
+      traits::propagate_on_container_move_assignment::value;
+  constexpr static bool pocs = traits::propagate_on_container_swap::value;
+  constexpr static bool always_equal = traits::is_always_equal::value;
+
+  // create, destroy, and copy are the same as before.
+
+  T* create(auto&& source) {
+    T* obj = traits::allocate(alloc_, 1);
+    try {
+      traits::construct(alloc_, obj, std::forward<decltype(source)>(source));
+    } catch (...) {
+      traits::deallocate(alloc_, obj, 1);
+      throw;
+    }
+
+    return obj;
+  }
+
+  void destroy(T* obj) {
+    if (obj == nullptr) {
+      return;
+    }
+    traits::destroy(alloc_, obj);
+    traits::deallocate(alloc_, obj, 1);
+  }
+
+  T* copy(const T* obj) {
+    if (obj == nullptr) {
+      return nullptr;
+    }
+    return create(*obj);
+  }
+
+  // We may have to actually perform a move
+  // for some cases now. This move constructs
+  // a new T using our allocator (NOT a simple
+  // pointer swap).
+  T* move(T* obj) {
+    if (obj == nullptr) {
+      return nullptr;
+    }
+    return create(std::move(*obj));
+  }
+
+ public:
+  Owner()
+    requires std::default_initializable<Alloc>
+  = default;
+
+  explicit Owner(const T& obj)
+    requires std::default_initializable<Alloc>
+      : obj_(create(obj)) {}
+
+  explicit Owner(T&& obj)
+    requires std::default_initializable<Alloc>
+      : obj_(create(std::move(obj))) {}
+
+  explicit Owner(std::allocator_arg_t, const Alloc& a, const T& obj)
+      : alloc_(a), obj_(create(obj)) {}
+
+  explicit Owner(std::allocator_arg_t, const Alloc& a, T&& obj)
+      : alloc_(a), obj_(create(std::move(obj))) {}
+
+  ~Owner() { destroy(obj_); }
+
+  Owner(const Owner& other)
+      : alloc_(traits::select_on_container_copy_construction(other.alloc_)),
+        obj_(copy(other.obj_)) {}
+
+  Owner(std::allocator_arg_t, const Alloc& a, const Owner& other)
+      : alloc_(a), obj_(copy(other.obj_)) {}
+
+  Owner(Owner&& other) noexcept
+      : alloc_(std::move(other.alloc_)),
+        obj_(std::exchange(other.obj_, nullptr)) {}
+
+  // Allocator-aware move construction. We can only statically guarantee
+  // noexcept if the allocators are always equal. Otherwise, we may have to
+  // perform a potentially-throwing allocation.
+  Owner(std::allocator_arg_t, const Alloc& a,
+        Owner&& other) noexcept(always_equal)
+      : alloc_(a) {
+    if (always_equal || alloc_ == other.alloc_) {
+      // Fast path: alloc_ can deallocate other.obj_ since it is
+      // equivalent to other.alloc_.
+      obj_ = std::exchange(other.obj_, nullptr);
+    } else {
+      // Slow path: alloc_ and other.alloc_ are not equivalent.
+      // We must newly move-construct our object.
+      obj_ = move(other.obj_);
+      other.destroy(other.obj_);
+      other.obj_ = nullptr;
+    }
+  }
+
+  Owner& operator=(const Owner& other) {
+    if (this != &other) {
+      destroy(obj_);
+      if constexpr (traits::propagate_on_container_copy_assignment::value) {
+        alloc_ = other.alloc_;
+      }
+      obj_ = copy(other.obj_);
+    }
+    return *this;
+  }
+
+  // See the code below for a justification of the noexcept specification.
+  Owner& operator=(Owner&& other) noexcept(always_equal || pocma) {
+    if (this != &other) {
+      destroy(obj_);  // Deallocate with the old allocator.
+
+      if constexpr (pocma) {
+        alloc_ = other.alloc_;
+      }
+
+      // When pocma is true, we propagate the allocator. It is therefore
+      // safe in that case to deallocate the original object with the new
+      // allocator. Otherwise, we must verify the allocators are always_equal or
+      // equivalent.
+      if (always_equal || pocma || alloc_ == other.alloc_) {
+        // Fast path.
+        obj_ = std::exchange(other.obj_, nullptr);
+      } else {
+        // Slow path: we must newly move-construct our object.
+        obj_ = move(other.obj_);
+        other.destroy(other.obj_);
+        other.obj_ = nullptr;
+      }
+    }
+    return *this;
+  }
+
+  // See the code below for a justification of the noexcept specification.
+  void swap(Owner& other) noexcept(always_equal || pocs) {
+    // Unlike move assignment, swap will NOT attempt to reallocate if the
+    // allocators are not equal. Instead, we require that the allocators
+    // are equivalent - if they are not, swaping is undefined behavior.
+    if constexpr (!always_equal && !pocs) {
+      assert(alloc_ == other.alloc_);
+    }
+
+    using std::swap;       // Prefer a user-defined swap function when possible.
+    if constexpr (pocs) {  // Propagate allocators on swap.
+      swap(alloc_, other.alloc_);
+    }
+    swap(obj_, other.obj_);
+  }
+
+  bool has_value() const { return obj_ != nullptr; }
+
+  T& get() {
+    assert(has_value());
+    return *obj_;
+  }
+
+  const T& get() const {
+    assert(has_value());
+    return *obj_;
+  }
+
+  const Alloc& get_allocator() const { return alloc_; }
+};
+
+}  // namespace xyz::tutorials::propagating_allocator
