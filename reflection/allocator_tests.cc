e281879b1aa4fd3c83989f3ef60b3c80c9459ea7
Ryan <ryan.keane051@gmail.com>
2026-08-14 11:29:28 -0400

    Added Tester struct

--- reflection/allocator_tests.cc
+++ reflection/allocator_tests.cc
@@ -4,6 +4,9 @@
 #include "tagged_allocator.h"
 #include "tracking_allocator.h"
 
+#include <cstddef>
+#include <vector>
+
 struct Blank {};
 
 struct NonCopyable {
@@ -16,12 +19,27 @@ struct NonCopyable {
   ~NonCopyable() = default;
 };
 
+// IMPORTANT: Allocation counting assumes that protocol is always deferring
+// to its allocator when creating a value. In reality, it should probably
+// implement small buffer optimization, and therefore we may find there are
+// fewer allocations than these tests suggest. To sidestep this, we can use
+// a type that is guaranteed to be larger than protocol.
+struct Tester {
+  int val;
+  Tester(int value) : val(value) {}
+
+  Tester(std::initializer_list<int>) {}
+private:
+  using P = xyz::protocol<Blank, xyz::TrackingAllocator<std::byte>>;
+  std::array<std::byte, sizeof(P)> padding;
+};
+
 TEST(ProtocolTest, ConstructionAllocs) {
   unsigned allocs{};
   unsigned deallocs{};
   {
     xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
-    xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc, 15};
+    xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc, Tester{15}};
     EXPECT_EQ(allocs, 1);
     EXPECT_EQ(deallocs, 0);
   }
@@ -35,10 +53,57 @@ TEST(ProtocolTest, InPlaceConstructionAllocs) {
   {
     xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
     xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc,
-                                            std::in_place_type<int>, 15};
+                                            std::in_place_type<Tester>, 15};
     EXPECT_EQ(allocs, 1);
     EXPECT_EQ(deallocs, 0);
   }
   EXPECT_EQ(allocs, 1);
   EXPECT_EQ(deallocs, 1);
+}
+
+TEST(ProtocolTest, InitListConstructionAllocs) {
+  unsigned allocs{};
+  unsigned deallocs{};
+  {
+    xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
+    xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc,
+                                            std::in_place_type<Tester>, {1, 2, 3}};
+    EXPECT_EQ(allocs, 1);
+    EXPECT_EQ(deallocs, 0);
+  }
+  EXPECT_EQ(allocs, 1);
+  EXPECT_EQ(deallocs, 1);
+}
+
+TEST(ProtocolTest, CopyConstructionAllocs) {
+  unsigned allocs{};
+  unsigned deallocs{};
+  {
+    xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
+    xyz::protocol<Blank, decltype(alloc)> p1{std::allocator_arg, alloc, Tester{25}};
+    xyz::protocol p2{p1};
+    EXPECT_EQ(allocs, 2);
+    EXPECT_EQ(deallocs, 0);
+  }
+  EXPECT_EQ(allocs, 2);
+  EXPECT_EQ(deallocs, 2);
+}
+
+TEST(ProtocolTest, CopyAssignmentAllocs) {
+  unsigned allocs{};
+  unsigned deallocs{};
+  {
+    xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
+    
+    xyz::protocol<Blank, decltype(alloc)> p1{std::allocator_arg, alloc, Tester{30}};
+    xyz::protocol p2{std::allocator_arg, alloc, Tester{40}};
+    EXPECT_EQ(allocs, 2);
+    EXPECT_EQ(deallocs, 0);
+
+    p1 = p2;
+    EXPECT_EQ(allocs, 3);
+    EXPECT_EQ(deallocs, 1);
+  }
+  EXPECT_EQ(allocs, 2);
+  EXPECT_EQ(deallocs, 2);
 }
