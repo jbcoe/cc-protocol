19d8e1315b14f5c92bd286a42961600d53f817ea
Ryan <ryan.keane051@gmail.com>
2026-08-14 11:15:07 -0400

    In place ctor test

--- reflection/allocator_tests.cc
+++ reflection/allocator_tests.cc
@@ -1,30 +1,44 @@
+#include <gtest/gtest.h>
+
 #include "protocol.h"
 #include "tagged_allocator.h"
 #include "tracking_allocator.h"
 
-#include <gtest/gtest.h>
-
 struct Blank {};
+
 struct NonCopyable {
-    NonCopyable(const NonCopyable&) = delete;
-    NonCopyable& operator=(const NonCopyable&) = delete;
+  NonCopyable(const NonCopyable&) = delete;
+  NonCopyable& operator=(const NonCopyable&) = delete;
 
-    NonCopyable(NonCopyable&&) = default;
-    NonCopyable& operator=(NonCopyable&&) = default;
+  NonCopyable(NonCopyable&&) = default;
+  NonCopyable& operator=(NonCopyable&&) = default;
 
-    ~NonCopyable() = default;
+  ~NonCopyable() = default;
 };
 
-TEST(ProtocolTest, ConstructionAllocations) {
-    unsigned allocs{};
-    unsigned deallocs{};
-    {
-        xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
-        xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc, 15};
-        EXPECT_EQ(allocs, 1);
-        EXPECT_EQ(deallocs, 0);
-    }
+TEST(ProtocolTest, ConstructionAllocs) {
+  unsigned allocs{};
+  unsigned deallocs{};
+  {
+    xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
+    xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc, 15};
     EXPECT_EQ(allocs, 1);
-    EXPECT_EQ(deallocs, 1);
+    EXPECT_EQ(deallocs, 0);
+  }
+  EXPECT_EQ(allocs, 1);
+  EXPECT_EQ(deallocs, 1);
 }
 
+TEST(ProtocolTest, InPlaceConstructionAllocs) {
+  unsigned allocs{};
+  unsigned deallocs{};
+  {
+    xyz::TrackingAllocator<std::byte> alloc{&allocs, &deallocs};
+    xyz::protocol<Blank, decltype(alloc)> p{std::allocator_arg, alloc,
+                                            std::in_place_type<int>, 15};
+    EXPECT_EQ(allocs, 1);
+    EXPECT_EQ(deallocs, 0);
+  }
+  EXPECT_EQ(allocs, 1);
+  EXPECT_EQ(deallocs, 1);
+}
