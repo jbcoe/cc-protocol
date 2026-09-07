f82ffea4e8b2cdd2f185296717ac0953055747b0
Ryan Keane <ryan.keane051@gmail.com>
2026-09-07 16:38:44 -0400

    Turn off unused filter for ConstProtocolCast

--- protocol_test.cc
+++ protocol_test.cc
@@ -2618,11 +2618,11 @@ TEST(ReflectionProtocolViewTest, ConstProtocolCast) {
   // NOLINTEND(readability-qualified-auto)
   static_assert(std::same_as<decltype(underlying), const Conforming&>);
 
-  // NOLINTBEGIN(readability-qualified-auto): the test demonstrates that
-  // the type of auto is indeed const; adding const would not exercise the
-  // behavior.
+  // NOLINTBEGIN(readability-qualified-auto,
+  // clang-analyzer-deadcode.DeadStores): the test demonstrates that the type of
+  // auto is indeed const; adding const would not exercise the behavior.
   auto* underlying_ptr = protocol_cast<Conforming>(&cv);
-  // NOLINTEND(readability-qualified-auto)
+  // NOLINTEND(readability-qualified-auto, clang-analyzer-deadcode.DeadStores)
   static_assert(std::same_as<decltype(underlying_ptr), const Conforming*>);
 }
 
