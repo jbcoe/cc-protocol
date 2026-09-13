453c62901316ad5a5602991bc44a0d5a1c044e8f
Jonathan B. Coe <jonathanbcoe@gmail.com>
2026-09-13 09:53:47 -0600

    Split detail into logical parts (#355)

---
+++ overload_spec.hh
@@ -0,0 +1,38 @@
+/* Copyright (c) 2025 The XYZ Protocol Authors. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+==============================================================================*/
+#ifndef XYZ_PROTOCOL_OVERLOAD_SPEC_HH_
+#define XYZ_PROTOCOL_OVERLOAD_SPEC_HH_
+
+#include <meta>
+
+namespace xyz::detail {
+
+template <typename R, typename... Args>
+using fn_ptr_t = R (*)(Args...);
+
+// One overload of a synthesised member function or operator: the interface
+// member (which names its vtable entry) and the const-qualification of the
+// generated wrapper.
+template <std::meta::info Member, bool IsConst>
+struct overload_spec {};
+
+}  // namespace xyz::detail
+
+#endif  // XYZ_PROTOCOL_OVERLOAD_SPEC_HH_
